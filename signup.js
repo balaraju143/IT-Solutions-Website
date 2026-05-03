b8d563bbb4ec5ef0c626cba4648d769f57b39926
balaraju143 <yarapatinibalaraju143@gmail.com>
2026-05-03 07:20:51 +0530

    first commit

--- signup.js
+++ signup.js
@@ -22,3 +22,25 @@ if (signupForm) {
     window.location.href = "login.html";
   });
 }
+
+
+// COUNTER ANIMATION
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  const update = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(update, 20);
+    } else {
+      counter.innerText = target + "+";
+    }
+  };
+
+  update();
+});
